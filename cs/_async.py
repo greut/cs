eea8debfe7c6604419d534f6dd38764bc5c7a786
Yoan Blanc <yoan.blanc@exoscale.ch>
2018-06-13 01:52:25 +0200

    travis: enable mypy checks
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- cs/_async.py
+++ cs/_async.py
@@ -1,5 +1,6 @@
 import asyncio
 import ssl
+from typing import Any, List  # noqa
 
 import aiohttp
 
@@ -31,7 +32,7 @@ class AIOCloudStack(CloudStack):
             handler = getattr(session, self.method)
 
             done = False
-            final_data = []
+            final_data = []  # type: List[Any]
             page = 1
             while not done:
                 if fetch_list:
